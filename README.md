9159c3bba9b92887b93e72c04dc065dc16820ceb
Elkana <elkanatuwei4@gmail.com>
2022-05-10 20:42:07 +0300

    Add README file

---
+++ README.md
@@ -0,0 +1,2 @@
+# Interview Assignment
+Receives an integer as GET parameter and returns a json response based on if the number is a multiple of five, seven, both five and seven or none.
--- main_app/views.py
+++ main_app/views.py
@@ -10,18 +10,25 @@ class MainAPIView(APIView):
 
     def get(self, request):
         integer_value = request.GET.get("integer")
+        # check to verify that the value is passed in the params
         if integer_value:
+            # parse integer value of parameter
             integer_value = int(integer_value)
             res = {}
+            # check if the integer is  multiple of both five and seven
             if (integer_value % 5 == 0) and (integer_value % 7 == 0):
                 res = {"data": "LR"}
+            # check if the integer is  multiple of five
             elif integer_value % 5 == 0:
                 res = {"data": "L"}
+            # check if the integer is  multiple of seven
             elif integer_value % 7 == 0:
                 res = {"data": "R"}
+            # return the integer value if none of the conditions is matched
             else:
                 res = {"data": integer_value}
             return Response(res, status=status.HTTP_200_OK)
         else:
+            # return error response if integer is not included in request
             res = {"message": "Provide Integer"}
             return Response(res, status=status.HTTP_400_BAD_REQUEST)
